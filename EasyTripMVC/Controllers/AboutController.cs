64146a21fd74da2c4770648469adfb2b6e64901c
erhangndz <erhangndz@live.com>
2023-05-08 13:34:16 +0300

    project finished

--- EasyTripMVC/Controllers/AboutController.cs
+++ EasyTripMVC/Controllers/AboutController.cs
@@ -15,5 +15,27 @@ namespace EasyTripMVC.Controllers
             var values = c.Abouts.ToList();
             return View(values);
         }
+
+        public ActionResult Index()
+        {
+            var values= c.Abouts.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult UpdateAbout(int id)
+        {
+            var values = c.Abouts.Find(id);
+            return View(values);
+        }
+        [HttpPost]
+        public ActionResult UpdateAbout(About p)
+        {
+            var x = c.Abouts.Find(p.AboutID);
+            x.ImageURL= p.ImageURL;
+            x.Description= p.Description;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- EasyTripMVC/Models/Classes/About.cs
+++ EasyTripMVC/Models/Classes/About.cs
@@ -10,5 +10,6 @@ namespace EasyTripMVC.Models.Classes
         public int AboutID { get; set; }
         public string ImageURL { get; set; }
         public string Description { get; set; }
+        
     }
 }
